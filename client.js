ba5c3437d576691424c445a8d2e7f55b05b0c37d
Unknown <ryu.shai112@gmail.com>
2017-09-18 14:10:06 +0700

    Server
    server - >received image (not encode) and save at hard  disk

---
+++ client.js
@@ -0,0 +1,30 @@
+var client = require('nodejs-websocket')
+//create file reader 
+var fs = require('fs');
+var buffer = new Buffer(0);
+var data;
+base64_encode('/home/shai/Pictures/test.jpg')
+// buffer = Buffer.concat([buffer,data],buffer.length+data.length)
+// console.log("ryu : "+data);
+
+// function to encode file data to base64 encoded string
+function base64_encode(file) {
+    // read binary data
+    var bitmap = fs.readFileSync(file);
+    // convert binary data to base64 encoded string
+    data= new Buffer(bitmap);
+    // return 'ryu'
+}
+
+
+var conn = client.connect('ws://localhost:8001',function(str){
+    console.log("connected: ");
+    // conn.sendText('ryu from client')
+    conn.on('text',function(str){
+        console.log('from server: '+ str);
+        conn.close();
+    })
+    conn.send(data,function(str){
+        console.log('send binary');
+    })
+})
--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 var ws = require("nodejs-websocket")
-
+var fs = require('fs')
 // Scream server example: "hi" -> "HI!!!"
+var data;
 var server = ws.createServer(function (conn) {
+
 	console.log("New connection")
 	conn.on("text", function (str) {
-		console.log("Received "+str)
+		console.log("Received ")
+		data+=str;
 		conn.sendText("hehe")
+		base64_decode(data,'received.jpg');
+		// fs.writeFile("receivedText.jpg",data);
 	})
 	conn.on("close", function (code, reason) {
 		console.log("Connection closed")
 	})
+
+	conn.on("binary",function(inStream){
+		console.log('receive binary: ')
+		var data = new Buffer(0);
+		inStream.on('readable',function(){
+			var newData = inStream.read();
+			if(newData){
+				data = Buffer.concat([data,newData],data.length+newData.length);
+			}
+		})
+
+		inStream.on('end',function(){
+			console.log('end of tranfer binary')
+			base64_decode(data,'received.jpg');
+		})
+	})
 }).listen(8001)
+
+function processMyData(data){
+	fs.writeFile("received.jpg",data,function(err){
+		console.log("error: "+err);
+	})
+}
+
+// function to create file from base64 encoded string
+function base64_decode(base64str, file) {
+    // create buffer object from base64 encoded string, it is important to tell the constructor that the string is base64 encoded
+    var bitmap = new Buffer(base64str);
+    // write buffer to file
+    fs.writeFileSync(file, base64str);
+    console.log('******** File created from base64 encoded string ********');
+}
